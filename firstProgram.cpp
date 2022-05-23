985c85bd2ee8aa1ab8533df288bd669f18e993d7
Nourhan Elsherif <66208099+nourhan-ashraf@users.noreply.github.com>
2022-05-23 15:45:46 -0700

    Update firstProgram.cpp

--- firstProgram.cpp
+++ firstProgram.cpp
@@ -1,15 +1,16 @@
+//Time complexity is O(E*log(E))
+
 #include <bits/stdc++.h>
 using namespace std;
-map<int,int>mapping;
-map<int,int>mapping2;
 
-//Time complexity is O(N*log(N))
-set<int>adjList[810000];
+map<int,int>mapping;  //map between the account and its index
+map<int,int>mapping2; //map between the index and its account
+set<int>adjList[810000]; //array (vertcies) of sets (set for each vertex)
 
 int k=0;
-void buildGraph(int follower, int account)   //O(log(N))
+void buildGraph(int follower, int account)   //O(log(E))
 {
-        if(!mapping.count(account)){    //map "count" takes O(log(N))
+        if(!mapping.count(account)){    //map "count" takes O(log(E))
             k++;
             mapping[account] = k;      //give each vertex an index beside its ID to be more easier to loop on each vertex 
             mapping2[k] = account;
@@ -18,27 +19,25 @@ void buildGraph(int follower, int account)   //O(log(N))
 }
 
 
-set<pair<int ,int>>ans;
-vector<pair<int, pair<int,int>>>v;
-//           index     id   size
-//           v.first,    v.second.first , v.second.second
+set<pair<int ,int>>ans;  //set of pairs of the ID and the size of each ID followers list sorted by the size 
+vector<pair<int, pair<int,int>>>v; //vector of pairs of pairs to map between three values (index, ID, size) sorted by the size in deascending order
+//           index     ,id             ,size
+//           v.first,  ,v.second.first ,v.second.second
 
 
-void printDegree(int num){
-    for (int i = 1; i <=81305; i++) {
+void printDegree(int num){  //O(V)
+    for (int i = 1; i <=81305; i++) {  //O(V)
         int lst = adjList[i].size();
         ans.insert(make_pair(lst,mapping2[i])); 
                           //size, id
     }
-   //(1000, 3197098), (500, 23618970), ....., etc
-
     int it = 81305;
-    for(auto u:ans){
+    for(auto u:ans){ //O(V)
         v.push_back(make_pair(it, make_pair(u.second, u.first)));  //u.second -> id, u.first -> size
         it--;
     }
-    for(auto j:v){
-        if(num==j.first){
+    for(auto j:v){  //O(V)
+        if(num==j.first){ 
             cout<<"Account ID: "<<j.second.first<<" , "<<"Followers: "<<j.second.second<<endl;
             break;
         }
@@ -48,12 +47,12 @@ void printDegree(int num){
 
 int main()
 {
-    ifstream myFile("C:\\Users\\Noura\\Downloads\\twitter (1).csv"); //open the data file
+    ifstream myFile("twitter.txt"); //open the data file
 
     if (myFile.is_open())  
     {
         string line;
-        while(getline(myFile,line)) //O(Nlog(N)) for N of lines of data
+        while(getline(myFile,line)) //O(Elog(E)) for N of lines of data
         {
             stringstream ss(line);
 			string x;
@@ -72,11 +71,14 @@ int main()
 		cout<<"Cannot open the file!"<<endl;
 		return 0;
 	}
-//cout<<k<<endl;
+
     cout<<"Enter the influncer order: "; //take the input from the user
 	int num; cin>>num;
-
-	printDegree(num); 
+	if(num<1 || num>81305){   //check if the input exceeds the number of accounts which is 81305 or it was smaller than 1
+		cout<<"Wrong input!"<<endl;
+		return 0;
+	}
+	printDegree(num);  //O(V)
 
 	return 0;
 }
