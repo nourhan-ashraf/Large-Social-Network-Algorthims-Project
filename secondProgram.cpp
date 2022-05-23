cfa3f696c56ca95072023d7ff5ddc3985720c308
Nourhan Elsherif <66208099+nourhan-ashraf@users.noreply.github.com>
2022-05-23 14:44:52 -0700

    Update secondProgram.cpp

--- secondProgram.cpp
+++ secondProgram.cpp
@@ -2,76 +2,61 @@
 using namespace std;
 map<int,int>mapping;
 
-//Time complexity is O(N*log(n))
-
-struct Graph {
-	int Vertcies; //number of vertices
-	unordered_set<int>* adjList;  //lists of vertices and each vertex has a list (unordered set) of followers
-};
-
-Graph* initializeGraph(int Vertcies) //utility function to intialize struct variables
-{
-	Graph* graph = new Graph;
-	graph->Vertcies = Vertcies;
-	graph->adjList = new unordered_set<int>[Vertcies];
-
-	return graph;
-}
+//Time complexity is O(N*log(N))
+set<int>adjList[810000];
 
 int k=0;
-void buildGraph(Graph* graph, int account, int following)   //O(log(n))
+void buildGraph(int account, int following)   //O(log(N))
 {
         
-        if(!mapping.count(account)){    //map "count" takes O(log(n))
+        if(!mapping.count(account)){    //map "count" takes O(log(N))
             k++;
             mapping[account] = k;      //give each vertex an index beside its ID to be more easier to loop on each vertex 
         }
-        graph->adjList[mapping[account]].insert(following); //add list of following to each vertex
-                                                            //unordered_set insert complexity is O(1)
+        adjList[mapping[account]].insert(following); //add list of following to each vertex
+                                                     //O(log(N))     
         
         //cout<<account<<" "<<mapping[account]<<endl;
     }
 	
 
-void searchAccounts(Graph* graph, int account)  //O(d*d))
+void searchAccounts(int account)  //O(d*log(d)))
 {
-		unordered_set<int> lst = graph->adjList[mapping[account]];  //list of following
+		set<int> lst = adjList[mapping[account]];  //list of following
 		cout <<"Accounts you may know: ";
         auto itr = lst.begin();
         int cnt=3;
         //O(3*d) -> worst case ,  d is the number of following in the following list
         while(itr != lst.end() && cnt!=0){  //loop on the following list of the account ID
-            unordered_set<int> follow = graph->adjList[mapping[*itr]]; //following list of each ID in the "user ID" the following list
+            set<int> follow = adjList[mapping[*itr]]; //following list of each ID in the "user ID" the following list
             int cnt2=3;
             for(auto d = follow.begin() ; d != follow.end() ;++d){  //for each person in the "user ID" following list loop on its following list to find an ID that the "user ID" doesn't follow
-                if( (!(graph->adjList[mapping[account]].count(*d))) && cnt2!=0 && (*d)!=account){ // if an ID is found and it's not equal to the "user ID", print it and decrease the counter to find other one
-                    // //unordered_set "count" complexity is O(1) -> avarage , O(d) -> worst case
+                if( (!(lst.count(*d))) && cnt2!=0 && (*d)!=account){ // if an ID is found and it's not equal to the "user ID", print it and decrease the counter to find other one
+                    //set "count" complexity is O(log(d))
                     cout << *d<<"  "; //print the suggestion
                     cnt2--;
-                }
-                cnt--;
-                ++itr;
+                } 
             }
+            cnt--;
+            ++itr;
 	    }
 
 }
 
 int main()
 {
-	int Vertcies = 810000;
-	struct Graph* graph = initializeGraph(Vertcies);
     /*buildGraph(graph, 4, 1);
     buildGraph(graph, 77, 77);
     buildGraph(graph, 8, 77);
     buildGraph(graph, 8, 4);
     buildGraph(graph, 99, 8);
     buildGraph(graph, 100, 4);*/
-    ifstream myFile("twitter.csv"); //open the data file
+    ifstream myFile("C:\\Users\\Noura\\Downloads\\twitter (1).csv"); //open the data file
 
     if (myFile.is_open())  
     {
         string line;
-        while(getline(myFile,line)) //O(Nlog(n)) for N of lines of data
+        while(getline(myFile,line)) //O(Nlog(N)) for N of lines of data
         {
             stringstream ss(line);
 			string x;
@@ -82,7 +67,7 @@ int main()
 			account = stoi(x);  //convert from string to int 
 			following = stoi(y);
 
-           buildGraph(graph, account, following); 
+           buildGraph(account, following); 
         }
 
     }
@@ -103,7 +88,7 @@ int main()
         }
     }
     if(flag){
-	    searchAccounts(graph,ID);  
+	    searchAccounts(ID);  
     }
     else{
         cout<<"This ID doesn't exist!"<<endl;
